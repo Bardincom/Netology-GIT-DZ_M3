667ce90bed2f0b267adefebc81ab7d85e4793883
Bardincom <master_als@icloud.com>
2020-01-21 00:02:32 +0200

    prepared for verification

--- Task1/Course2Week3Task1/Code/CalculatorViewController.swift
+++ Task1/Course2Week3Task1/Code/CalculatorViewController.swift
@@ -5,10 +5,6 @@
 //  Copyright © 2018 e-Legion. All rights reserved.
 //
 
-
-///Приложение должно принимать от пользователя два операнда. Значения UIStepper от 1 до 10 включительно и кратны 0.5.
-///Второе UISlider от 1 до 100 включительно.
-
 import UIKit
 
 class CalculatorViewController: UIViewController {
@@ -29,45 +25,17 @@ class CalculatorViewController: UIViewController {
     settingInterface()
   }
   
-//  TO DO настройка цвета для Stepper
-  func settingInterface() {
-    view.backgroundColor = .backgroundColor
-    resultLable.backgroundColor = .lableBackgroundColor
-    resultLable.textColor = .textResultColor
-    resultLable.text = "1"
-    textLable.forEach{$0.textColor = .otherTextLableColor}
-    sliderOutlet.tintColor = .actionElementsColor
-    buttonOutlet.backgroundColor = .actionElementsColor
-    buttonOutlet.setTitleColor(.otherTextLableColor, for: .normal)
-    buttonOutlet.setTitle("Calculate", for: .normal)
-  }
-  
-  func initialValueStepper() {
-    stepperOutlet.minimumValue = 1
-    stepperOutlet.maximumValue = 10
-    stepperOutlet.stepValue = 0.5
-    firstOperand.text = numberFormatterStepper(stepperOutlet.value)
-  }
-  
-  func initialValueSlider() {
-    sliderOutlet.minimumValue = 1
-    sliderOutlet.maximumValue = 100
-    secondOperand.text = numberFormatterSlider(sliderOutlet.value)
-  }
-  
   @IBAction func settingValueStepper(_ sender: UIStepper) {
-    firstOperand.text = numberFormatterStepper(sender.value)
+    firstOperand.text = setValueOperand(NSNumber(value: sender.value))
   }
   
   @IBAction func settingValueSlider(_ sender: UISlider) {
-    secondOperand.text = numberFormatterSlider(sender.value)
+    secondOperand.text = setValueOperand(NSNumber(value: sender.value))
   }
   
   @IBAction func pressCalculateButton(_ sender: UIButton) {
-    let numberFormatter = NumberFormatter()
-    numberFormatter.maximumFractionDigits = 4
-    
-    resultLable.text = numberFormatter.string(from: NSNumber(value: stepperOutlet.value * Double(sliderOutlet.value)))
+    resultLable.text = multiplication(stepperOutlet.value, sliderOutlet.value)
   }
   
 }
+
--- Task1/Course2Week3Task1/Code/Extention + CalculatorViewController.swift
+++ Task1/Course2Week3Task1/Code/Extention + CalculatorViewController.swift
@@ -9,26 +9,56 @@
 import UIKit
 
 extension CalculatorViewController {
-    func numberFormatterStepper(_ value: Double) -> String? {
-      let numberFormatter = NumberFormatter()
-      numberFormatter.minimumFractionDigits = 4
-      return numberFormatter.string(from: NSNumber(value: value))
-    }
   
-  func numberFormatterSlider(_ value: Float) -> String? {
-      let numberFormatter = NumberFormatter()
-      numberFormatter.minimumFractionDigits = 4
-     return numberFormatter.string(from: NSNumber(value: value))
-    }
+  func settingInterface() {
+    view.backgroundColor = .backgroundColor
+    
+    resultLable.backgroundColor = .lableBackgroundColor
+    resultLable.textColor = .textResultColor
+    resultLable.text = "1"
+    
+    textLable.forEach{$0.textColor = .otherTextLableColor}
+    
+    sliderOutlet.tintColor = .actionElementsColor
+    
+    stepperOutlet.layer.borderWidth = 1
+    stepperOutlet.layer.cornerRadius = 5
+    stepperOutlet.layer.borderColor = UIColor.actionElementsColor.cgColor
+    stepperOutlet.tintColor = .actionElementsColor
+    stepperOutlet.setDecrementImage(stepperOutlet.decrementImage(for: .normal), for: .normal)
+    stepperOutlet.setIncrementImage(stepperOutlet.incrementImage(for: .normal), for: .normal)
+    stepperOutlet.tintColor = .actionElementsColor
+    
+    buttonOutlet.backgroundColor = .actionElementsColor
+    buttonOutlet.setTitleColor(.otherTextLableColor, for: .normal)
+    buttonOutlet.setTitle("Calculate", for: .normal)
+  }
   
-//  func numberFormatter<T>(_ value: T) -> String? where T: UISlider {
-//    let numberFormatter = NumberFormatter()
-//    numberFormatter.minimumFractionDigits = 4
-//    
-//    
-//    return numberFormatter.string(from: NSNumber(value: value.value))
-//  }
-//  
-//
+  func initialValueStepper() {
+    stepperOutlet.minimumValue = 1
+    stepperOutlet.maximumValue = 10
+    stepperOutlet.stepValue = 0.5
+    firstOperand.text = setValueOperand(NSNumber(value: stepperOutlet.value))
+  }
+  
+  func initialValueSlider() {
+    sliderOutlet.minimumValue = 1
+    sliderOutlet.maximumValue = 100
+    secondOperand.text = setValueOperand(NSNumber(value: sliderOutlet.value))
+  }
+  
+  func setValueOperand(_ value: NSNumber) -> String? {
+    let numberFormatter = NumberFormatter()
+    numberFormatter.minimumFractionDigits = 4
+    
+    return numberFormatter.string(from: value)
+  }
+  
+  func multiplication(_ firstOperand: Double, _ secondOperand: Float ) -> String? {
+    let numberFormatter = NumberFormatter()
+    numberFormatter.maximumFractionDigits = 4
+    
+    return numberFormatter.string(from: NSNumber(value: firstOperand * Double(secondOperand)))
+  }
 
 }
