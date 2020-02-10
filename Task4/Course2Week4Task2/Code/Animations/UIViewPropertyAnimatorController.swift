a9f1c5cafb48a29c8309a039ce51a17afbed196f
Bardincom <master_als@icloud.com>
2020-02-10 20:45:30 +0200

    UIViewPropertyAnimatorController is done

--- Task4/Course2Week4Task2/Code/Animations/UIViewPropertyAnimatorController.swift
+++ Task4/Course2Week4Task2/Code/Animations/UIViewPropertyAnimatorController.swift
@@ -6,35 +6,48 @@
 //
 
 import UIKit
-/*
- Следующий тип анимации - UIViewPropertyAnimator, которой соответствует UIViewPropertyAnimatorController.
- У этого контроллера имеется один аутлет animationView.
- Кроме того, на корневой view добавлен UIPanGestureRecognizer.
- Горизонтальное перемещение пальца по корневому view должно вызывать анимированное перемещение animationView вслед за пальцем.
- Перемещение должно происходить вдоль оси X с одновременным поворотом вокруг оси Z и изменением масштаба animationView.
- После удаления пальца с экрана возможны две ситуации:
- - если х-координата центра animationView меньше х-координаты центра корневого view, то animationView должен анимированно вернуться в первоначальное положение.
- - В противном случае animationView анимированно движется в конечную точку анимации.
- 
- Подсказка: для определения центра animationView используйте свойство position presentation слоя.
- Параметры анимации следующие:
-  - центр animationView должен переместиться вдоль оси Х на 300pt относительно своего первоначального положения,
-  -  сам animationView должен повернуться на 180° вокруг оси Z по часовой стрелке и его масштаб должен увеличиться в 1.5 раза.
-  - В качестве timingParameters нужно использовать UISpringTimingParameters со следующими значениями:
-  - mass: 2.0,
-  - stiffness: 30.0,
-  - damping: 7.0,
-  - initialVelocity: .zero.
- */
 
 class UIViewPropertyAnimatorController: UIViewController {
-    
-    @IBOutlet weak var animationView: UIView!
-    
-    @IBAction func panGestureHandler(recognizer: UIPanGestureRecognizer) {
-    }
   
-  func fingerFollowing() {
-    
+  @IBOutlet weak var animationView: UIView!
+  
+  var animator = UIViewPropertyAnimator()
+  
+  let timingParameters: UISpringTimingParameters = .init(mass: 2.0,
+                                                         stiffness: 30.0,
+                                                         damping: 7.0,
+                                                         initialVelocity: .zero)
+  
+  @IBAction func panGestureHandler(recognizer: UIPanGestureRecognizer) {
+    fingerFollowing(recognizer)
+  }
+  
+  func fingerFollowing(_ recognizer: UIPanGestureRecognizer) {
+    switch recognizer.state{
+      case .began:
+        animator = UIViewPropertyAnimator(duration: 0,
+                                          timingParameters: timingParameters)
+        
+        self.animationView.transform = .init(rotationAngle: .pi)
+        
+        animator.addAnimations {
+          self.animationView.layer.position.x += 300
+          self.animationView.transform = .init(scaleX: 1.5, y: 1.5)
+        }
+        
+        animator.pauseAnimation()
+      case .changed:
+        animator.fractionComplete = recognizer.translation(in: view).x / view.bounds.size.width
+      case .ended:
+        if self.animationView.layer.position.x < self.view.center.x {
+          animator.isReversed = false
+        } else {
+          animator.isReversed = true
+        }
+        
+        animator.startAnimation()
+      default:
+        ()
+    }
   }
 }
