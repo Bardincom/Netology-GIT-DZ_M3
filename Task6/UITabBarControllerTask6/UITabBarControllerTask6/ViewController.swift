00c7aa318c1ae15972177930c2c5c3bb3e9a7942
Bardincom <master_als@icloud.com>
2020-02-16 22:52:34 +0200

    v. 0.1 Initial Task6

---
+++ Task6/UITabBarControllerTask6/UITabBarControllerTask6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITabBarControllerTask6
+//
+//  Created by Aleksey Bardin on 16.02.2020.
+//  Copyright © 2020 Aleksey Bardin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
